9e3a87ae6fa19178a9445dffd14cc37614ee7a20
danielsouzacargoblue-jpg <danielsouzacargoblue@gmail.com>
2026-06-04 08:04:58 -0300

    Update 02_TORRE_MONITORAMENTO.py

--- pages/02_TORRE_MONITORAMENTO.py
+++ pages/02_TORRE_MONITORAMENTO.py
@@ -10,6 +10,27 @@ st.set_page_config(
     layout="wide"
 )
 
+USUARIO = "monitoramento"
+SENHA = "Shopee2026"
+
+if "logado" not in st.session_state:
+    st.session_state.logado = False
+
+if not st.session_state.logado:
+    st.title("🔐 Login")
+
+    usuario = st.text_input("Usuário")
+    senha = st.text_input("Senha", type="password")
+
+    if st.button("Entrar"):
+        if usuario == USUARIO and senha == SENHA:
+            st.session_state.logado = True
+            st.rerun()
+        else:
+            st.error("Usuário ou senha inválidos")
+
+    st.stop()
+
 st_autorefresh(interval=60000, key="refresh")
 
 st.markdown("""
